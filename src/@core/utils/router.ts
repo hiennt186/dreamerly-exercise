b2e6234d92a90c3c94d00d87fa44c4e464e09d12
Hien Trong Nguyen <nguyenhien.meek@gmail.com>
2023-09-17 20:01:06 +0700

    fix: build failed

--- src/@core/utils/router.ts
+++
@@ -1,4 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { createRouter } from 'next-connect'
-
-export const router = () => createRouter<NextApiRequest, NextApiResponse>()
